b0c8fb55486916c80bb02f783431b3d76533643e
Pramitha0512 <pramitha.bonaboina@gmail.com>
2025-03-13 13:28:18 +0530

    Add files via upload

---
+++ LAB3/l3_switchdemo2.dart
@@ -0,0 +1,16 @@
+class l3_switchdemo2{
+void colors(String c){
+		switch(c){
+			case 'red' || 'RED' || 'Red' :
+	     			print("Red is danger");
+				break;
+			case 'orange' || 'ORANGE' || 'Orange' :
+				print("orange is present");
+				break;
+			case _ :
+				print("Sorry, this color is not available");
+		
+		
+				}
+			}
+}
