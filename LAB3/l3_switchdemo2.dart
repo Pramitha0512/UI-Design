1e34bd0dc589e3144aeb38ed3be9b194e011a5f3
Pramitha0512 <pramitha.bonaboina@gmail.com>
2025-03-13 13:46:23 +0530

    Add files via upload

--- LAB3/l3_switchdemo2.dart
+++ LAB3/l3_switchdemo2.dart
@@ -1,16 +1,24 @@
-class l3_switchdemo2{
-void colors(String c){
-		switch(c){
-			case 'red' || 'RED' || 'Red' :
-	     			print("Red is danger");
-				break;
-			case 'orange' || 'ORANGE' || 'Orange' :
-				print("orange is present");
-				break;
-			case _ :
-				print("Sorry, this color is not available");
-		
-		
-				}
-			}
+class L3SwitchDemo2 {
+  void sdemo(String name) {
+    goto: 
+      switch (name) {
+        case 'apple':
+          print('This is apple');
+          break goto; 
+        case 'banana':
+          print('This is banana');
+          break goto; 
+        case 'mango':
+          print('This is mango');
+          break goto; 
+        case 'grapes':
+          print('This is grapes');
+          break goto; 
+        default:
+          print('Sorry, fruit is not available in the list');
+          break goto; 
+      }
+    }
+  }
 }
+
